abe18cd434b32e923692222a4d04b24adb9d77d8
lisapeters13 <lisapetersirl@gmail.com>
2024-07-02 16:45:50 -0400

    sending work to github

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  const p = document.getElementById("text")
+  console.log(p.textContent) 
+  document.getElementById("text").textContent = "This is really cool!"
+  ;
+  });
+//   const p = document.getElementById("text")
+// console.log(p.textContent) 
+// document.getElementById("text").textContent = "This is really cool!"
+// ;
+  
